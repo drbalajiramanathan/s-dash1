250836f220ec73b034f194b634937ae547729162
Balaji Ramanathan <drbalaji96@gmail.com>
2026-01-11 19:23:37 -0500

    Fix: Update Firebase Project ID to mooflux-f0a46

--- deploy_dash1.bat
+++ deploy_dash1.bat
@@ -5,5 +5,5 @@ call npm run build -- --base=/dash1/ --outDir=dist/dash1 --emptyOutDir
 cd ..
 echo.
 echo Build complete. Deploying to Firebase...
-firebase deploy
+firebase deploy --project mooflux-f0a46
 pause
